efcb755c3051577534201846d9dcfbb1bf6a56aa
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2021-11-08 13:07:56 -0400

    Eureka client anotation

--- src/main/java/com/arquitecturasoftware/msstudent/MsStudentApplication.java
+++ src/main/java/com/arquitecturasoftware/msstudent/MsStudentApplication.java
@@ -2,8 +2,10 @@ package com.arquitecturasoftware.msstudent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsStudentApplication {
 
 	public static void main(String[] args) {
